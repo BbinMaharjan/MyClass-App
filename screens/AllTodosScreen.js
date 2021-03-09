20c8c2bc2de07641115c4755f83ff10424edef8e
BbinMaharjan <bbinmaharjan@gmail.com>
2021-03-09 12:14:06 +0545

    todos mark as complete done

--- screens/AllTodosScreen.js
+++ screens/AllTodosScreen.js
@@ -1,13 +1,34 @@
 import React from "react";
 import { View, StyleSheet } from "react-native";
-import { Appbar, Modal, TextInput, Checkbox, Button } from "react-native-paper";
+import {
+  Appbar,
+  Modal,
+  TextInput,
+  Checkbox,
+  Button,
+  List,
+  Paragraph,
+  Dialog,
+  Portal,
+  Text,
+} from "react-native-paper";
 import { useTheme } from "@react-navigation/native";
+import { useDispatch, useSelector } from "react-redux";
+import { addTodos, markAsComplete } from "../store/actions/todos";
 
 const AllTodosScreen = (props) => {
   const [visible, setVisible] = React.useState(false);
   const [title, setTitle] = React.useState("");
   const [description, setDescription] = React.useState("");
+  const [selectedTodoId, setSelectedTodoId] = React.useState("");
   const [priority, setPriority] = React.useState(false);
+
+  const [openDialog, setOpenDialog] = React.useState(false);
+
+  const hideDialog = () => setOpenDialog(false);
+
+  const dispatch = useDispatch();
+  const todos = useSelector((state) => state.todosState.todos);
 
   const showModal = () => setVisible(true);
   const hideModal = () => {
@@ -17,6 +38,23 @@ const AllTodosScreen = (props) => {
     setVisible(false);
   };
 
+  const submitForm = () => {
+    const todo = {
+      id: Math.random().toString(),
+      title,
+      description,
+      priority,
+      isComplete: false,
+    };
+    dispatch(addTodos(todo));
+    hideModal();
+  };
+
+  const handleListTap = (id) => {
+    setOpenDialog(true);
+    setSelectedTodoId(id);
+  };
+
   const { colors } = useTheme();
   return (
     <View style={{ flex: 1 }}>
@@ -24,6 +62,29 @@ const AllTodosScreen = (props) => {
         <Appbar.Content titleStyle={{ color: "#fff" }} title="All Todos" />
         <Appbar.Action icon="plus" color="#fff" size={30} onPress={showModal} />
       </Appbar.Header>
+      {todos.map((todo) => {
+        return (
+          <List.Item
+            onPress={() => handleListTap(todo.id)}
+            key={todo.id}
+            title={todo.title}
+            description={todo.description}
+            left={(props) => (
+              <List.Icon
+                {...props}
+                icon={todo.isComplete ? "check" : "clipboard-list"}
+              />
+            )}
+            right={(props) =>
+              todo.priority ? (
+                <List.Icon {...props} icon={"priority-high"} />
+              ) : (
+                <View></View>
+              )
+            }
+          />
+        );
+      })}
 
       <Modal
         visible={visible}
@@ -72,15 +133,29 @@ const AllTodosScreen = (props) => {
           >
             Cancel
           </Button>
-          <Button
-            icon="content-save"
-            mode="contained"
-            onPress={() => console.log("Pressed")}
-          >
+          <Button icon="content-save" mode="contained" onPress={submitForm}>
             Save
           </Button>
         </View>
       </Modal>
+      <Portal>
+        <Dialog visible={openDialog} onDismiss={hideDialog}>
+          <Dialog.Title>
+            Do you want to mark this todo as complete?
+          </Dialog.Title>
+          <Dialog.Actions>
+            <Button onPress={hideDialog}>Cancel</Button>
+            <Button
+              onPress={() => {
+                dispatch(markAsComplete(selectedTodoId));
+                setOpenDialog(false);
+              }}
+            >
+              Yes
+            </Button>
+          </Dialog.Actions>
+        </Dialog>
+      </Portal>
     </View>
   );
 };
--- screens/CompletedTodosScreen.js
+++ screens/CompletedTodosScreen.js
@@ -1,10 +1,14 @@
 import { useTheme } from "@react-navigation/native";
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
-import { Appbar } from "react-native-paper";
+import { Appbar, List } from "react-native-paper";
+import { useSelector } from "react-redux";
 
 const CompletedTodosScreen = (props) => {
   const { colors } = useTheme();
+  const allTodos = useSelector((state) => state.todosState.todos);
+  const completedTodos = allTodos.filter((todo) => todo.isComplete);
+
   return (
     <View style={{ flex: 1 }}>
       <Appbar.Header style={{ backgroundColor: colors.primary }}>
@@ -13,6 +17,28 @@ const CompletedTodosScreen = (props) => {
           title="Completed"
         />
       </Appbar.Header>
+      {completedTodos.map((todo) => {
+        return (
+          <List.Item
+            key={todo.id}
+            title={todo.title}
+            description={todo.description}
+            left={(props) => (
+              <List.Icon
+                {...props}
+                icon={todo.isComplete ? "check" : "clipboard-list"}
+              />
+            )}
+            right={(props) =>
+              todo.priority ? (
+                <List.Icon {...props} icon={"priority-high"} />
+              ) : (
+                <View></View>
+              )
+            }
+          />
+        );
+      })}
     </View>
   );
 };
